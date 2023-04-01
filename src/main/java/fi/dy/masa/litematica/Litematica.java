b650d64187f03adf8c1ba131cdab421accb5c9d1
Forgot-Dream <1360169078@qq.com>
2023-04-01 13:14:33 +0800

    Init Commit. Fork and modified from https://github.com/DreamRealmCraft/litematica-forge.

---
+++ src/main/java/fi/dy/masa/litematica/Litematica.java
@@ -0,0 +1,26 @@
+package fi.dy.masa.litematica;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import fi.dy.masa.malilib.event.InitializationHandler;
+import fi.dy.masa.litematica.config.Configs;
+
+@Mod(Reference.MOD_ID)
+public class Litematica
+{
+    public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
+
+    public Litematica()
+    {
+        InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
+    }
+
+    public static void debugLog(String msg, Object... args)
+    {
+        if (Configs.Generic.DEBUG_LOGGING.getBooleanValue())
+        {
+            Litematica.logger.info(msg, args);
+        }
+    }
+}
